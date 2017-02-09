11eb049cddd827dc86e92a41829edfe03a01e515
musha <musha@air2.local>
2017-02-09 11:24:32 +0900

    ch01-03確認

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Go Web Programming の例題です。
 準備中です。
 
+01 first_webapp ok
+
+02 chitchat ok
+
 
--- ch03/01simplest/server.go
+++ ch03/01simplest/server.go
@@ -4,15 +4,16 @@
   $ sudo go run server.go  
 
  * ほかのウェブサーバが動いているときは、すぐに終了してしまいます。
- * ブラウザに http://localhost/ を入力すると "404 page not found" が表示されます。
+ * 起動しておいてブラウザに http://localhost/ を入力すると "404 page not found" が表示されます。
    それ以上の機能はありません。
+	 終了するには control+C を押してください。
 
 なお、次のコマンドを実行すると01simplest という実行ファイルができます。
   $ go build
 その後で次のコマンドを実行するとサーバが起動します。
  sudo ./01simplest
 
-さらに、次のコマンドを実行すると $GOPATH/bin に01simplestという実行ファイルができます。
+また、次のコマンドを実行すると $GOPATH/bin に01simplestという実行ファイルができます。
   $ go install
 
 （$GOPATH はインストール時に設定したGO関連のファイいるが置かれる場所を示す環境変数）
