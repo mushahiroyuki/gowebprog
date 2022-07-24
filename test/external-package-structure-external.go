37aaaf97fdc8f8c1bb902c8915ec19cef1eb191c
musha hiroyuki <hmusha@gmail.com>
2022-07-24 12:21:45 +0900

    update test

--- test/external-package-structure-external.go
+++ test/external-package-structure-external.go
@@ -1,11 +1,24 @@
 package external
 
-struct ExternalStructure {
+import("fmt")
+
+type ExternalStructure struct {
 	External int
 	internal string
 }
 
-ExternVar ExternalStructure {
-	External 10
-	internal "文字列"
+var ExternVar ExternalStructure
+
+func init() {
+	ExternVar.External  = 10
+	ExternVar.internal  = "文字列"
+}
+
+
+func ExternalTest() {
+	fmt.Println("ExternalTest")
+}
+
+func internalTest() {
+	fmt.Println("internalTest")
 }
